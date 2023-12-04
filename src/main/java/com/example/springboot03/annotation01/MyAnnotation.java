30453b9f8b3409699b23a23bef68b19f2c6a21a1
githubyh2 <githubyh2@github.com>
2023-12-04 16:59:41 +0800

    2023/12/4-1 master 分支增加 自定义annotation注解的备注信息

--- src/main/java/com/example/springboot03/annotation01/MyAnnotation.java
+++ src/main/java/com/example/springboot03/annotation01/MyAnnotation.java
@@ -11,8 +11,9 @@ package com.example.springboot03.annotation01;
  * 2、内部定义成员，通常使用 value 表示
  * 3、可以指定成员默认值，使用defaule 定义
  * 4、如果自定义的注解没有成员，表名是一个标识作用
- *
+ * <p>
  * 如果注解有成员，在使用注解的时候，需要指明成员值
+ * 自定义注解必须配上注解的信息处理流程（使用反射） 才有意义
  */
 public @interface MyAnnotation {
     String value() default "hello";
